0be8c0e9cd5c048f2abaf922e0985353595ba8f2
mrynp <110476086+mrynp@users.noreply.github.com>
2024-08-21 23:29:09 +0800

    Change div to dialog

--- app.js
+++ app.js
@@ -2,11 +2,21 @@ const newBook = document.querySelector(".new-book");
 const formContainer = document.querySelector(".form-container");
 
 newBook.addEventListener("click", () => {
-  formContainer.classList.add("fadeIn");
+  dialog.showModal();
 });
 
 const closeBtn = document.querySelector(".close");
 
 closeBtn.addEventListener("click", () => {
-  formContainer.classList.remove("fadeIn");
+  dialog.close();
 });
+
+const books = document.querySelector(".books");
+const myLibrary = [];
+
+function Book(title, author, pages, status) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.status = status;
+}
