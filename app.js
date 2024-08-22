2ef8681279e752c852435425ef1b7a7eb916aa25
mrynp <110476086+mrynp@users.noreply.github.com>
2024-08-22 13:54:27 +0800

    Display book when added

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const formContainer = document.querySelector(".form-container");
 const dialogElem = document.getElementById("dialog");
 const addBook = document.querySelector(".add-book");
 const bookForm = document.querySelector("#book-form");
+const bookContainer = document.querySelector(".book-container");
 
 newBook.addEventListener("click", () => {
   dialogElem.showModal();
@@ -46,5 +47,16 @@ bookForm.addEventListener("submit", (e) => {
 
 function addBookToLibrary(title, author, pages, status) {
   myLibrary.push(new Book(title, author, pages, status));
-  console.log(myLibrary);
+  displayBook();
+}
+
+function displayBook() {
+  bookContainer.innerHTML = "";
+  for (const [index, book] of myLibrary.entries()) {
+    const { title, author, pages, read } = book;
+    bookContainer.innerHTML += `<div class="book">
+                                    <p class="title">${title}</h2>
+                                    <p class="author">${author}</h3
+                                    </div>`;
+  }
 }
