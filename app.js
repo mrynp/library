0739c2533ba978b931dfaf33a8b2b6c9670b9079
mrynp <110476086+mrynp@users.noreply.github.com>
2024-08-22 13:43:27 +0800

    Push new book to mylibrary

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 const newBook = document.querySelector(".new-book");
 const formContainer = document.querySelector(".form-container");
+const dialogElem = document.getElementById("dialog");
+const addBook = document.querySelector(".add-book");
+const bookForm = document.querySelector("#book-form");
 
 newBook.addEventListener("click", () => {
-  dialog.showModal();
+  dialogElem.showModal();
 });
 
 const closeBtn = document.querySelector(".close");
 
 closeBtn.addEventListener("click", () => {
-  dialog.close();
+  dialogElem.close();
 });
 
-const books = document.querySelector(".books");
+const book = document.querySelector(".book");
 const myLibrary = [];
 
 function Book(title, author, pages, status) {
@@ -19,4 +22,29 @@ function Book(title, author, pages, status) {
   this.author = author;
   this.pages = pages;
   this.status = status;
+  this.id = Math.floor(Math.random() * 1000000);
+}
+
+bookForm.addEventListener("submit", (e) => {
+  e.preventDefault;
+  const formData = new FormData(e.target);
+  const formDataObj = Object.fromEntries(formData);
+
+  let title = formDataObj.title;
+  let author = formDataObj.author;
+  let pages = formDataObj.pages;
+  let status;
+
+  if (formDataObj.status === "on") {
+    status = "read";
+  } else {
+    status = "notRead";
+  }
+
+  addBookToLibrary(title, author, pages, status);
+});
+
+function addBookToLibrary(title, author, pages, status) {
+  myLibrary.push(new Book(title, author, pages, status));
+  console.log(myLibrary);
 }
